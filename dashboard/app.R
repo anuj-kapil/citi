3757e647d6e6d1cb105ba2477660769c29a70380
anuj-kapil <45342051+anuj-kapil@users.noreply.github.com>
2018-12-07 08:52:14 +1100

    Update app.R

--- dashboard/app.R
+++ dashboard/app.R
@@ -104,6 +104,16 @@ sidebar <- dashboardSidebar(
   ),
   sidebarMenuOutput(outputId = "menu"),
   conditionalPanel(condition = "input.tabbox1 === 'charts'",
+                   # selectInput(inputId = "industry", label = "Industry:",
+                   #             choices = c("Food" = 41,
+                   #                         "Household Goods" = 42,
+                   #                         "Clothing" = 43,
+                   #                         "Department Stores" = 44,
+                   #                         "Others" = 45,
+                   #                         "Cafes & Restaurants" = 46),
+                   #             selected = c("Food" = 41)),
+                   # dateInput(inputId = "date1", label = "Start Date:", value = "2008-01-01"),
+                   # dateInput(inputId = "date2", label = "End Date:", value = Sys.Date())
                    selectInput(inputId = "industry", label = "Industry:",
                                choices = c("Food" = 41,
                                            "Household Goods" = 42,
@@ -112,8 +122,11 @@ sidebar <- dashboardSidebar(
                                            "Others" = 45,
                                            "Cafes & Restaurants" = 46),
                                selected = c("Food" = 41)),
-                   dateInput(inputId = "date1", label = "Start Date:", value = "2008-01-01"),
-                   dateInput(inputId = "date2", label = "End Date:", value = Sys.Date())
+                   selectInput(inputId = "seriesType1", label = "Series Type:",
+                               choices = c("Original" = 10,
+                                           "Seasonal" = 20,
+                                           "Trend" = 30),
+                               selected = c("Seasonal" = 20))
   )
   ,
   conditionalPanel(condition = "input.tabbox1 === 'data'",
@@ -155,7 +168,15 @@ body <- dashboardBody(
                            column(width = 4, infoBoxOutput("infoBox3", width = 12)),
                            column(width = 4, infoBoxOutput("infoBox2", width = 12))
                          )),
-                tabPanel("Charts", value = "charts", highchartOutput("distPlot", width = "100%" , height = "700px"))
+                tabPanel("Charts", value = "charts", 
+                         fluidRow(
+                           plotOutput("distPlot", width = "100%" , height = "700px")
+                         ),
+                         fluidRow( 
+                           #column(width = 4, infoBoxOutput("infoBox1", width = 12)),
+                           #column(width = 4, infoBoxOutput("infoBox3", width = 12)),
+                           column(width = 4, infoBoxOutput("infoBox4", width = 12))
+                         ))
               ))
     )
   )
@@ -184,7 +205,15 @@ ui <- dashboardPage(header, sidebar, body)
 
 
 server <- function(input, output, session) {
-  #setwd("/Users/anuj/Documents/UTS/Citi")
+  setwd("/Users/anuj/Documents/UTS/Citi")
+  local <- TRUE
+  if(local){
+    filep <- "RT_Monthly_Forecast.csv"
+  }
+  else{
+    filep <- "https://raw.githubusercontent.com/anuj-kapil/citi/master/data/RT_Monthly_Forecast.csv"
+  }
+  print(filep)
   reactive_data <- reactiveFileReader(
     intervalMillis = 1000000,
     session = session,
@@ -197,7 +226,7 @@ server <- function(input, output, session) {
   reactive_data_forecast <- reactiveFileReader(
     intervalMillis = 1000000,
     session = session,
-    filePath = "https://raw.githubusercontent.com/anuj-kapil/citi/master/data/RT_Monthly_Forecast.csv",
+    filePath = filep,
     readFunc = function(filePath){
       fread(filePath, sep = ",")
     }
@@ -207,7 +236,7 @@ server <- function(input, output, session) {
     dt <- dt[IND_R == input$industry1 & TSEST == input$seriesType]
     dt <- tail(dt[order(obsTime)], 5)
     namesList <-  c("obsTime", "C20","C20_ActualGrowth" , "C20_Adjusted","C20_AdjustedGrowth", "C20_AdjErr", "C20Pred", "C20PredGrowth", "C20_PredErr", "C20_F", "C20_FGrowth", "C20_FErr")
-    namesListDisp <-  c("Date","Actual Revenue($M)", "Actual Growth(%)", "Pred.Revenue($M)","Pred.Growth(%)", "Pred.Err(%)", "Linear.Rev($M)","Linear.Growth(%)", "Linear.Err(%)", "Forecast.Rev($M)","Forecast.Growth(%)", "Forecast.Err(%)")
+    namesListDisp <-  c("Date","Actual Revenue($M)", "Actual Growth(%)", "Pred Revenue($M)","Pred Growth(%)", "Pred Err(%)", "Linear Rev($M)","Linear Growth(%)", "Linear Err(%)", "Forecast Rev($M)","Forecast Growth(%)", "Forecast Err(%)")
     dt <- dt[,namesList, with = FALSE]
     setorderv(dt,"obsTime")
     lastrow <- nrow(dt)
@@ -284,7 +313,7 @@ server <- function(input, output, session) {
       #dt[, errorrate := (abs(C20_Adjusted-C20)*100)/C20]
       #namesList <-  c("obsTime", "C20","C20_ActualGrowth" , "C20_Adjusted","C20_AdjustedGrowth", "C20_AdjErr", "C20Pred", "C20PredGrowth", "C20_PredErr", "C20_F", "C20_FGrowth", "C20_FErr")
       cols <- names(dt)[names(dt) != 'obsTime']
-      dt[ , (cols) := lapply(.SD, function(x){formatC(x, big.mark = ",", format = "f", digits = 2)}, -1), .SDcols = cols]
+      dt[ , (cols) := lapply(.SD, function(x){formatC(x, big.mark = ",", format = "f", digits = 2)}), .SDcols = cols]
       # dt[, errorrate := formatC(errorrate, big.mark = ",", format = "f", digits = 2)]
       # dt[, C20 := formatC(C20, big.mark = ",", format = "f", digits = 2)]
       # dt[, C20_Adjusted := formatC(C20_Adjusted, big.mark = ",", format = "f", digits = 2)]
@@ -299,7 +328,7 @@ server <- function(input, output, session) {
       output$infoBox3 <- renderInfoBox({
         infoBox("", "0%" , icon = icon("arrow-circle-up"), fill = TRUE, color = "yellow")
       })
-      dt[, errorrate := NA]
+      #dt[, errorrate := NA]
     }
     names(dt) <- namesListDisp
     return(dt)
@@ -312,72 +341,110 @@ server <- function(input, output, session) {
     paste0("",names(choiceVec2)[choiceVec2 == input$seriesType])
   })
   
+  output$distPlot <- renderPlot({
+  dt <- reactive_data_forecast()
+  dt <- dt[IND_R == input$industry & TSEST == input$seriesType1]
+  # Enable below code to chech the correlation
+  namesList <-  c("obsTime", "C20","C30","C20_ActualGrowth","C30_ActualGrowth")
+  plotData <- dt[,namesList, with = FALSE]
+  mydata <- na.omit(plotData[,2:3]) # listwise deletion of missing
+  mydata <- scale(mydata)
+  dt_core <- cor(mydata, use = "pairwise.complete.obs")
+  dt_core <- round(dt_core*100,2)
+  #dt_core <- formatC(dt_core, big.mark = ",", format = "f", digits = 2)
+  output$infoBox4 <- renderInfoBox({
+    infoBox("Corr(%)", dt_core , icon = icon("arrow-circle-up"), fill = TRUE, color = "yellow")
+  })
+  # Total 98.86%
+  # 41 98.53%
+  # 42 98.61%
+  # 43 98.74%
+  # 44 97.30%
+  # 45 98.88%
+  # 46 97.64%
   
-  output$distPlot <- renderHighchart({
-    dt <- reactive_data()
-    dt[,obsTime := as.Date(paste(obsTime, "01", sep = "-"))]
-    dt[,TSEST := as.factor(TSEST)]
-    dt <- dt[IND_R == input$industry & obsTime >= input$date1 & obsTime <= input$date2]
-    title_txt <- paste0("Retail Trade Industry - ",names(choiceVec)[choiceVec == input$industry], "\n")
-    hchart(dt, 'line', hcaes(x = 'obsTime', y = 'obsValue', group = "TSEST" ), lineWidth = 1, backgroundColor = "transparent") %>%
-      hc_title(text = title_txt) %>%
-      hc_xAxis(title = list(text = "Date")) %>%
-      hc_yAxis(title = list(text = "Revenue ($ million)")) %>%
-      hc_legend(labelFormatter = JS("function () {
-                                    switch (this.name) {
-                                    case '10':
-                                    val = 'Original';
-                                    break;
-                                    case '20':
-                                    val = 'Seasonal';
-                                    break;
-                                    case '30':
-                                    val = 'Trend';
-                                    }
-                                    return val;
-  }"))%>%
-      hc_tooltip(shared = TRUE, formatter = JS("function () {
-                                               var d = new Date(this.x)
-                                               var month = new Array(12);
-                                               month[0] = 'January';
-                                               month[1] = 'February';
-                                               month[2] = 'March';
-                                               month[3] = 'April';
-                                               month[4] = 'May';
-                                               month[5] = 'June';
-                                               month[6] = 'July';
-                                               month[7] = 'August';
-                                               month[8] = 'September';
-                                               month[9] = 'October';
-                                               month[10] = 'November';
-                                               month[11] = 'December';
-                                               var n = month[d.getUTCMonth()];
-                                               var o = new Date(this.x).getUTCFullYear()
-                                               
-                                               function seriesNameLoookup(caseval) {
-                                               switch (caseval) {
-                                               case '10':
-                                               val = 'Original';
-                                               break;
-                                               case '20':
-                                               val = 'Seasonal';
-                                               break;
-                                               case '30':
-                                               val = 'Trend';
-                                               }
-                                               return val;
-                                               }
-                                               
-                                               arrLen = this.points.length;
-                                               text = '<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />';
-                                               text = text + '<b>' + n + ' '+ o  + '</b><br>'
-                                               for (i = 0; i < arrLen; i++) {
-                                               text += '<span style=\"color:'+ this.points[i].point.color +'\"><p>\u25CF <p></span>' + seriesNameLoookup(this.points[i].point.series.name) + ': <b>' + this.points[i].y + '</b><br>'
-                                               }
-                                               return text;
-      }"))%>%
-      hc_add_theme(hc_theme_custom)
-    })
+  # Visually fit a linear regression
+  
+  
+    ggplot(plotData,aes(x=C20, y=C30))+
+      geom_point(color = '#60636a')+
+      geom_smooth(method = "lm", color = 'Orange')+
+      labs(title="Linear Regression\n", size = 15)+
+      theme(panel.background = element_blank(), axis.line = element_line(colour = "grey"), plot.title = element_text(hjust = 0.5))+
+      scale_x_continuous(name="Retail Trade", labels = scales::comma)+
+      scale_y_continuous(name="Labour Force", labels = scales::comma)+
+      scale_fill_tableau()+
+      theme(axis.text.x = element_text(size=8),
+            axis.text.y = element_text(size=8))
+  }, res = 150)
+  
+  
+  
+  # output$distPlot <- renderHighchart({
+  #   dt <- reactive_data()
+  #   dt[,obsTime := as.Date(paste(obsTime, "01", sep = "-"))]
+  #   dt[,TSEST := as.factor(TSEST)]
+  #   dt <- dt[IND_R == input$industry & obsTime >= input$date1 & obsTime <= input$date2]
+  #   title_txt <- paste0("Retail Trade Industry - ",names(choiceVec)[choiceVec == input$industry], "\n")
+  #   hchart(dt, 'line', hcaes(x = 'obsTime', y = 'obsValue', group = "TSEST" ), lineWidth = 1, backgroundColor = "transparent") %>%
+  #     hc_title(text = title_txt) %>%
+  #     hc_xAxis(title = list(text = "Date")) %>%
+  #     hc_yAxis(title = list(text = "Revenue ($ million)")) %>%
+  #     hc_legend(labelFormatter = JS("function () {
+  #                                   switch (this.name) {
+  #                                   case '10':
+  #                                   val = 'Original';
+  #                                   break;
+  #                                   case '20':
+  #                                   val = 'Seasonal';
+  #                                   break;
+  #                                   case '30':
+  #                                   val = 'Trend';
+  #                                   }
+  #                                   return val;
+  # }"))%>%
+  #     hc_tooltip(shared = TRUE, formatter = JS("function () {
+  #                                              var d = new Date(this.x)
+  #                                              var month = new Array(12);
+  #                                              month[0] = 'January';
+  #                                              month[1] = 'February';
+  #                                              month[2] = 'March';
+  #                                              month[3] = 'April';
+  #                                              month[4] = 'May';
+  #                                              month[5] = 'June';
+  #                                              month[6] = 'July';
+  #                                              month[7] = 'August';
+  #                                              month[8] = 'September';
+  #                                              month[9] = 'October';
+  #                                              month[10] = 'November';
+  #                                              month[11] = 'December';
+  #                                              var n = month[d.getUTCMonth()];
+  #                                              var o = new Date(this.x).getUTCFullYear()
+  #                                              
+  #                                              function seriesNameLoookup(caseval) {
+  #                                              switch (caseval) {
+  #                                              case '10':
+  #                                              val = 'Original';
+  #                                              break;
+  #                                              case '20':
+  #                                              val = 'Seasonal';
+  #                                              break;
+  #                                              case '30':
+  #                                              val = 'Trend';
+  #                                              }
+  #                                              return val;
+  #                                              }
+  #                                              
+  #                                              arrLen = this.points.length;
+  #                                              text = '<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />';
+  #                                              text = text + '<b>' + n + ' '+ o  + '</b><br>'
+  #                                              for (i = 0; i < arrLen; i++) {
+  #                                              text += '<span style=\"color:'+ this.points[i].point.color +'\"><p>\u25CF <p></span>' + seriesNameLoookup(this.points[i].point.series.name) + ': <b>' + this.points[i].y + '</b><br>'
+  #                                              }
+  #                                              return text;
+  #     }"))%>%
+  #     hc_add_theme(hc_theme_custom)
+  #   })
   
   }
 shinyApp(ui, server)
