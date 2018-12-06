bf58efdfa88db3f67637341bb7df82a9fe7e405e
anuj-kapil <45342051+anuj-kapil@users.noreply.github.com>
2018-12-07 08:53:16 +1100

    Update ReduceLag.R

--- R/ReduceLag.R
+++ R/ReduceLag.R
@@ -1,5 +1,5 @@
 ##### Installing Packages ###########
-install.packages('rsdmx')
+#install.packages('rsdmx')
 
 ##### Load Packages ###########
 library(rsdmx)
@@ -44,150 +44,159 @@ setDT(df)
 
 dt_out <- lapply(c(20,30), function(y){
   dt_inner <- lapply(listIndustries, function(x){
-  dt <- df[IND_R == x & TSEST == y, 7:8]
-  dt[,IND_R := 20]
-  dt[,obsTime := as.Date(paste(obsTime, "01", sep = "-"))]
-  setcolorder(dt, c("IND_R", "obsTime", "obsValue"))
-  
-  df_combined <- rbindlist(list(dt,dt_lf))
-  df_combined_cast <- dcast(df_combined, obsTime ~ IND_R, fill = NA, value.var = "obsValue")
-  setDT(df_combined_cast)
-  names(df_combined_cast) <- c("obsTime" ,"C20","C30" )
-  
-  # Enable below code to chech the correlation
-  
-  #mydata <- na.omit(df_combined_cast[,2:3]) # listwise deletion of missing
-  #mydata <- scale(mydata)
-  #df_trend_cast_core <- cor(mydata, use = "pairwise.complete.obs")
-  # Total 98.86%
-  # 41 98.53%
-  # 42 98.61%
-  # 43 98.74%
-  # 44 97.30%
-  # 45 98.88%
-  # 46 97.64%
-  
-  setDT(df_combined_cast)
-  #df_combined_cast[,C20Growth:= (C20-shift(C20, type = "lag", n = 1))/shift(C20, type = "lag", n = 1)]
-  #df_combined_cast[,C30Growth:= (C30-shift(C30, type = "lag", n = 1))/shift(C30, type = "lag", n = 1)]
-  
-  # Visually fit a linear regression
-  
-  # ggplot(df_combined_cast,aes(x=C20, y=C30))+
-  #   geom_point(color = '#60636a')+
-  #   geom_smooth(method = "lm", color = 'Orange')+
-  #   labs(title="Linear Regression\n", size = 15)+
-  #   theme(panel.background = element_blank(), axis.line = element_line(colour = "grey"), plot.title = element_text(hjust = 0.5))+
-  #   scale_x_continuous(name="Retail Trade", labels = scales::comma)+
-  #   scale_y_continuous(name="Labour Force", labels = scales::comma)+
-  #   scale_fill_tableau()+
-  #   theme(axis.text.x = element_text(size=8),
-  #         axis.text.y = element_text(size=8))
-  
-  # modlss <- loess(C20 ~ C30, data = df_combined_cast)
-  # fit <- arrange(augment(modlss), C30)
-  # 
-  # library(highcharter)
-  # highchart() %>% 
-  #   hc_add_series(df_combined_cast, type = "scatter",
-  #                 hcaes(x = C30, y = C20)) %>%
-  #   hc_add_series(fit, type = "spline", hcaes(x = C30, y = .fitted),
-  #                 name = "Fit", id = "fit") %>% 
-  #   hc_add_series(fit, type = "arearange",
-  #                 hcaes(x = C30, low = .fitted - 2*.se.fit,
-  #                       high = .fitted + 2*.se.fit),
-  #                 linkedTo = "fit")%>%
-  #   hc_add_theme(hc_theme_custom)
-  
-  linear_model <- lm(C20~C30, data = df_combined_cast )
-  df_combined_cast$C20Pred <- predict(linear_model, df_combined_cast[,3])
-  df_combined_cast[,C20PredGrowth:= (C20Pred-shift(C20Pred, type = "lag", n = 1))/shift(C20Pred, type = "lag", n = 1)]
-  
-  df_combined_cast[,C20PredGrowthR:= round(C20PredGrowth*100, 2)]
-  
-  #   x <- runif(400, -1,1)
-  #   y <- x*rnorm(400, mean = .25, sd = .5)
-  #   
-  #   sample_dt <- data.table(vx = x, vy = y)
-  #   ggplot(data = sample_dt, aes(x=vx, y=vy))+
-  #     geom_point(color = '#60636a')+
-  #     geom_smooth(method = "lm", color = 'Orange')+
-  #     labs(title="Linear Regression\n", size = 15)+
-  #     theme(panel.background = element_blank(), axis.line = element_line(colour = "grey"), plot.title = element_text(hjust = 0.5))+
-  #     scale_x_continuous(name="Retail Trade Growth", labels = scales::comma)+
-  #     scale_y_continuous(name="Labour Force Growth", labels = scales::comma)+
-  #     scale_fill_tableau()+
-  #     theme(axis.text.x = element_text(size=8),
-  #           axis.text.y = element_text(size=8))
-  # 
-  # 
-  # linear_model_s <- lm(vx~vy, data = sample_dt )
-  # sample_dt$vxPred <- predict(linear_model_s, sample_dt[,2])
-  # 
-  # min_max_accuracy <- mean(apply(sample_dt[, c("vx","vxPred"), with = FALSE],1, min)/apply(sample_dt[, c("vx","vxPred"), with = FALSE],1, max), na.rm = T)
-  # mape <- mean(abs((sample_dt$vxPred - sample_dt$vx))/sample_dt$vx, na.rm = T)
-  # 
-  
-  #min_max_accuracy <- mean(apply(actuals_preds, 1, min) / apply(actuals_preds, 1, max)) 
-  
-  #min_max_accuracy <- mean(apply(df_combined_cast[, c("C20","C20Pred"), with = FALSE],1, min)/apply(df_combined_cast[, c("C20","C20Pred"), with = FALSE],1, max), na.rm = T)
-  #mape <- mean(abs((df_combined_cast$C20Pred - df_combined_cast$C20))/df_combined_cast$C20, na.rm = T)
-  
-  #summary(linear_model)
-  #options(scipen = 999)
-  #geom_point()
-  rowNum <- nrow(df_combined_cast)
-  
-  fit <- auto.arima(head(df_combined_cast$C20, rowNum-5), seasonal = F)
-  fcast <- forecast(fit, h=5)
-  # test <- as.data.table(fcast)
-  # str(test)
-  # 
-  # summary(fcast)
-  # fcast$lower[,2]
-  dt_new <- df_combined_cast
-  dt_new[(rowNum-4):rowNum,C20_Adjusted := (fcast$upper[,1] * .8)+(C20Pred * .2) ]
-  dt_new[(rowNum-4):rowNum,C20_F := fcast$mean[1]]
-  dt_new[(rowNum-4):rowNum,C20_F_80_U := fcast$upper[1,1]]
-  dt_new[(rowNum-4):rowNum,C20_F_80_L := fcast$lower[1,1]]
-  dt_new[(rowNum-4):rowNum,C20_F_95_U := fcast$upper[1,2]]
-  dt_new[(rowNum-4):rowNum,C20_F_95_L := fcast$lower[1,2]]
-  dt_new[, C20_Adjusted:= as.numeric(C20_Adjusted)]
-  dt_new[,C20_AdjustedGrowth:= round(((C20_Adjusted-shift(C20_Adjusted, type = "lag", n = 1))/shift(C20_Adjusted, type = "lag", n = 1))*100,2)]
-  dt_new[,C20_ActualGrowth:= round(((C20-shift(C20, type = "lag", n = 1))/shift(C20, type = "lag", n = 1))*100,2)]
-  dt_new[,C20_FGrowth:= round(((C20_F-shift(C20_F, type = "lag", n = 1))/shift(C20_F, type = "lag", n = 1))*100,2)]
-  dt_new[,C20_PredErr:= round((abs(C20Pred - C20)*100)/C20,2)]
-  dt_new[,C20_AdjErr:= round((abs(C20_Adjusted - C20)*100)/C20,2)]
-  dt_new[,C20_FErr:= round((abs(C20_F - C20)*100)/C20,2)]
-  
-  dt_new[, IND_R := x]
-  dt_new[, TSEST := y]
-  return(dt_new)
-  # d <- max(df_combined_cast$obsTime) %m+% months(1)
-  # 
-  # df_combined_cast$C20_F <- NA
-  # df_combined_cast$C20_F_80_U <- NA
-  # df_combined_cast$C20_F_80_L <- NA
-  # df_combined_cast$C20_F_95_U <- NA
-  # df_combined_cast$C20_F_95_L <- NA
-  # 
-  # #names(df_combined_cast)%like%'C20'
-  # 
-  # dt_extra <- setNames(data.table(matrix(nrow = 12, ncol = 11)), names(df_combined_cast)) 
-  # dt_extra[, names(dt_extra) := lapply(.SD, as.numeric)]
-  # dt_extra[,obsTime := seq(as.Date(d), length=12, by="1 month")]
-  # dt_extra[,C20 := fcast$upper[,1]]
-  # dt_extra[,C20_F := fcast$mean]
-  # dt_extra[,C20_F_80_U := fcast$upper[,1]]
-  # dt_extra[,C20_F_80_L := fcast$lower[,1]]
-  # dt_extra[,C20_F_95_U := fcast$upper[,2]]
-  # dt_extra[,C20_F_95_L := fcast$lower[,2]]
-  # cols <- names(dt_extra)[names(dt_extra) != 'obsTime']
-  # dt_extra[ , (cols) := lapply(.SD, as.numeric, -1), .SDcols = cols]
-  # df_combined_cast[, (cols) := lapply(.SD, as.numeric, -1), .SDcols = cols]
-  # dt_new <- rbindlist(list(df_combined_cast, dt_extra))
-  
-})
+    #print(x)
+    #print(y)
+    dt <- df[IND_R == x & TSEST == y, 7:8]
+    dt[,IND_R := 20]
+    dt[,obsTime := as.Date(paste(obsTime, "01", sep = "-"))]
+    setcolorder(dt, c("IND_R", "obsTime", "obsValue"))
+    
+    df_combined <- rbindlist(list(dt,dt_lf))
+    df_combined_cast <- dcast(df_combined, obsTime ~ IND_R, fill = NA, value.var = "obsValue")
+    setDT(df_combined_cast)
+    names(df_combined_cast) <- c("obsTime" ,"C20","C30" )
+    
+    # Enable below code to chech the correlation
+    
+    #mydata <- na.omit(df_combined_cast[,2:3]) # listwise deletion of missing
+    #mydata <- scale(mydata)
+    #df_trend_cast_core <- cor(mydata, use = "pairwise.complete.obs")
+    # Total 98.86%
+    # 41 98.53%
+    # 42 98.61%
+    # 43 98.74%
+    # 44 97.30%
+    # 45 98.88%
+    # 46 97.64%
+    
+    setDT(df_combined_cast)
+    #df_combined_cast[,C20Growth:= (C20-shift(C20, type = "lag", n = 1))/shift(C20, type = "lag", n = 1)]
+    #df_combined_cast[,C30Growth:= (C30-shift(C30, type = "lag", n = 1))/shift(C30, type = "lag", n = 1)]
+    
+    # Visually fit a linear regression
+    
+    # ggplot(df_combined_cast,aes(x=C20, y=C30))+
+    #   geom_point(color = '#60636a')+
+    #   geom_smooth(method = "lm", color = 'Orange')+
+    #   labs(title="Linear Regression\n", size = 15)+
+    #   theme(panel.background = element_blank(), axis.line = element_line(colour = "grey"), plot.title = element_text(hjust = 0.5))+
+    #   scale_x_continuous(name="Retail Trade", labels = scales::comma)+
+    #   scale_y_continuous(name="Labour Force", labels = scales::comma)+
+    #   scale_fill_tableau()+
+    #   theme(axis.text.x = element_text(size=8),
+    #         axis.text.y = element_text(size=8))
+    
+    # modlss <- loess(C20 ~ C30, data = df_combined_cast)
+    # fit <- arrange(augment(modlss), C30)
+    # 
+    # library(highcharter)
+    # highchart() %>% 
+    #   hc_add_series(df_combined_cast, type = "scatter",
+    #                 hcaes(x = C30, y = C20)) %>%
+    #   hc_add_series(fit, type = "spline", hcaes(x = C30, y = .fitted),
+    #                 name = "Fit", id = "fit") %>% 
+    #   hc_add_series(fit, type = "arearange",
+    #                 hcaes(x = C30, low = .fitted - 2*.se.fit,
+    #                       high = .fitted + 2*.se.fit),
+    #                 linkedTo = "fit")%>%
+    #   hc_add_theme(hc_theme_custom)
+    
+    linear_model <- lm(C20~C30, data = df_combined_cast )
+    df_combined_cast$C20Pred <- predict(linear_model, df_combined_cast[,3])
+    df_combined_cast[,C20PredGrowth:= (C20Pred-shift(C20Pred, type = "lag", n = 1))/shift(C20Pred, type = "lag", n = 1)]
+    
+    df_combined_cast[,C20PredGrowthR:= round(C20PredGrowth*100, 2)]
+    
+    #   x <- runif(400, -1,1)
+    #   y <- x*rnorm(400, mean = .25, sd = .5)
+    #   
+    #   sample_dt <- data.table(vx = x, vy = y)
+    #   ggplot(data = sample_dt, aes(x=vx, y=vy))+
+    #     geom_point(color = '#60636a')+
+    #     geom_smooth(method = "lm", color = 'Orange')+
+    #     labs(title="Linear Regression\n", size = 15)+
+    #     theme(panel.background = element_blank(), axis.line = element_line(colour = "grey"), plot.title = element_text(hjust = 0.5))+
+    #     scale_x_continuous(name="Retail Trade Growth", labels = scales::comma)+
+    #     scale_y_continuous(name="Labour Force Growth", labels = scales::comma)+
+    #     scale_fill_tableau()+
+    #     theme(axis.text.x = element_text(size=8),
+    #           axis.text.y = element_text(size=8))
+    # 
+    # 
+    # linear_model_s <- lm(vx~vy, data = sample_dt )
+    # sample_dt$vxPred <- predict(linear_model_s, sample_dt[,2])
+    # 
+    # min_max_accuracy <- mean(apply(sample_dt[, c("vx","vxPred"), with = FALSE],1, min)/apply(sample_dt[, c("vx","vxPred"), with = FALSE],1, max), na.rm = T)
+    # mape <- mean(abs((sample_dt$vxPred - sample_dt$vx))/sample_dt$vx, na.rm = T)
+    # 
+    
+    #min_max_accuracy <- mean(apply(actuals_preds, 1, min) / apply(actuals_preds, 1, max)) 
+    
+    #min_max_accuracy <- mean(apply(df_combined_cast[, c("C20","C20Pred"), with = FALSE],1, min)/apply(df_combined_cast[, c("C20","C20Pred"), with = FALSE],1, max), na.rm = T)
+    #mape <- mean(abs((df_combined_cast$C20Pred - df_combined_cast$C20))/df_combined_cast$C20, na.rm = T)
+    
+    #summary(linear_model)
+    #options(scipen = 999)
+    #geom_point()
+    rowNum <- nrow(df_combined_cast)
+    
+    fit <- auto.arima(head(df_combined_cast$C20, rowNum-5), seasonal = F)
+    fcast <- forecast(fit, h=5)
+    # test <- as.data.table(fcast)
+    # str(test)
+    # 
+    # summary(fcast)
+    # fcast$lower[,2]
+    dt_new <- df_combined_cast
+    dt_new[(rowNum-4):rowNum,C20_Adjusted := (fcast$mean * .8)+(C20Pred * .2) ]
+    dt_new[(rowNum-4):rowNum,C20_F := fcast$mean]
+    dt_new[(rowNum-4):rowNum,C20_F_80_U := fcast$upper[,1]]
+    dt_new[(rowNum-4):rowNum,C20_F_80_L := fcast$lower[,1]]
+    dt_new[(rowNum-4):rowNum,C20_F_95_U := fcast$upper[,2]]
+    dt_new[(rowNum-4):rowNum,C20_F_95_L := fcast$lower[,2]]
+    dt_new[,C20_Adjusted:= as.numeric(C20_Adjusted)]
+    dt_new[,C20_F:= as.numeric(C20_F)]
+    dt_new[,C20_F_80_U:= as.numeric(C20_F_80_U)]
+    dt_new[,C20_F_80_L:= as.numeric(C20_F_80_L)]
+    dt_new[,C20_F_95_U:= as.numeric(C20_F_95_U)]
+    dt_new[,C20_F_95_L:= as.numeric(C20_F_95_L)]
+    dt_new[,C20_AdjustedGrowth:= round(((C20_Adjusted-shift(C20_Adjusted, type = "lag", n = 1))/shift(C20_Adjusted, type = "lag", n = 1))*100,2)]
+    dt_new[,C20_ActualGrowth:= round(((C20-shift(C20, type = "lag", n = 1))/shift(C20, type = "lag", n = 1))*100,2)]
+    dt_new[,C20_FGrowth:= round(((C20_F-shift(C20_F, type = "lag", n = 1))/shift(C20_F, type = "lag", n = 1))*100,2)]
+    dt_new[,C20_PredErr:= round((abs(C20Pred - C20)*100)/C20,2)]
+    dt_new[,C20_AdjErr:= round((abs(C20_Adjusted - C20)*100)/C20,2)]
+    dt_new[,C20_FErr:= round((abs(C20_F - C20)*100)/C20,2)]
+    dt_new[,C30_ActualGrowth:= round(((C30-shift(C30, type = "lag", n = 1))/shift(C30, type = "lag", n = 1))*100,2)]
+    
+    
+    dt_new[, IND_R := x]
+    dt_new[, TSEST := y]
+    return(dt_new)
+    # d <- max(df_combined_cast$obsTime) %m+% months(1)
+    # 
+    # df_combined_cast$C20_F <- NA
+    # df_combined_cast$C20_F_80_U <- NA
+    # df_combined_cast$C20_F_80_L <- NA
+    # df_combined_cast$C20_F_95_U <- NA
+    # df_combined_cast$C20_F_95_L <- NA
+    # 
+    # #names(df_combined_cast)%like%'C20'
+    # 
+    # dt_extra <- setNames(data.table(matrix(nrow = 12, ncol = 11)), names(df_combined_cast)) 
+    # dt_extra[, names(dt_extra) := lapply(.SD, as.numeric)]
+    # dt_extra[,obsTime := seq(as.Date(d), length=12, by="1 month")]
+    # dt_extra[,C20 := fcast$upper[,1]]
+    # dt_extra[,C20_F := fcast$mean]
+    # dt_extra[,C20_F_80_U := fcast$upper[,1]]
+    # dt_extra[,C20_F_80_L := fcast$lower[,1]]
+    # dt_extra[,C20_F_95_U := fcast$upper[,2]]
+    # dt_extra[,C20_F_95_L := fcast$lower[,2]]
+    # cols <- names(dt_extra)[names(dt_extra) != 'obsTime']
+    # dt_extra[ , (cols) := lapply(.SD, as.numeric, -1), .SDcols = cols]
+    # df_combined_cast[, (cols) := lapply(.SD, as.numeric, -1), .SDcols = cols]
+    # dt_new <- rbindlist(list(df_combined_cast, dt_extra))
+    
+  })
   dt_inner <- rbindlist(dt_inner)
   return(dt_inner)
 })
@@ -197,7 +206,7 @@ dt_out <- lapply(c(20,30), function(y){
 #   hc_add_series_labels_values(labels = c('A', ''), values = c(98, 2), color = c('red', 'black'), size = '100%', innerSize = "60%", showInLegend = T)
 
 dt_out <- rbindlist(dt_out)
-setwd("D:/Rio/ABS")
+setwd("/Users/anuj/Documents/UTS/Citi")
 fwrite(dt_out, 'RT_Monthly_Forecast.csv', sep = ",")
 # mape <- mean(abs((df_combined_cast$C20Pred - df_combined_cast$C20))/df_combined_cast$C20, na.rm = T)
 # 
